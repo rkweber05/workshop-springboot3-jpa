6948994f8cb6ccfd0696f1f8b93c69d881759213
rkweber05 <rkweber05@outlook.com>
2024-03-07 09:11:23 -0300

    Service laye, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entites.User;
+import com.educandoweb.course.repositories.UserRepositoty;
+
+@Service //registra a classe como componente do Spring
+public class UserService {
+	
+	@Autowired
+	private UserRepositoty repositoty;
+	
+	public List<User> findAll(){
+		return repositoty.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repositoty.findById(id);
+		
+		return obj.get(); // retorna o objeto do tipo user que estiver dentro do optional
+	}
+}
