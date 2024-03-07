32900d48153cdfe9e4e0bade5a2bc59dc9ecdb8d
rkweber05 <rkweber05@outlook.com>
2024-03-07 07:36:55 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entites/User.java
+++ src/main/java/com/educandoweb/course/entites/User.java
@@ -1,11 +1,21 @@
 package com.educandoweb.course.entites;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
-	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//estrategia de auto-encremento
 	private Long id;
 	private String name;
 	private String email;
