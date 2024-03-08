5fab294c932596a9f03dd76124ba50554095f5cb
rkweber05 <rkweber05@outlook.com>
2024-03-08 11:18:38 -0300

    Product

--- src/main/java/com/educandoweb/course/entites/Product.java
+++ src/main/java/com/educandoweb/course/entites/Product.java
@@ -5,8 +5,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,26 +12,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
-//anotações são mapeamento do JPA
-
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
-	@Transient // impede que o JPA tente interpretar ela
-	private Set<Product> products = new HashSet<>(); //set é um conjunto
+	@Transient
+	private Set<Category> categories = new HashSet<>(); // set define que não pode ter mesma categoria/ um produto não pode ter mesma categoria
 	
-	public Category() {}
+	public Product() {}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -51,9 +53,33 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
 	
-	public Set<Product> getProducts() {
-		return products;
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -69,11 +95,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-
-	
 }
