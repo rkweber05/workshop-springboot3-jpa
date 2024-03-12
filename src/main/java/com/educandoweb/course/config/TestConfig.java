3d2e047a8c220c250af6e180efa613bc598f3ec8
rkweber05 <rkweber05@outlook.com>
2024-03-12 07:43:02 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entites.Category;
 import com.educandoweb.course.entites.Order;
 import com.educandoweb.course.entites.OrderItem;
+import com.educandoweb.course.entites.Payment;
 import com.educandoweb.course.entites.Product;
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.entites.enums.OrderStatus;
@@ -79,6 +80,10 @@ public class TestConfig implements CommandLineRunner{ // classe para instanciar 
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1); // associei meu order1 com o meu payment1
+		orderRepository.save(o1); // depois de associar, salvar no banco 
 	}
 	
 	
