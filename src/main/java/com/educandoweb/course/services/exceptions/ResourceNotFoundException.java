a6f63e60e5f1f163f3246b4e03c326daf929653b
rkweber05 <rkweber05@outlook.com>
2024-03-12 15:43:03 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(Object id) { // tentar passar o id do obj que eu tentar encontrar, mas não encontrei
-		super("Resource not found. Id " + id);
+		super("Resource not found. Id " + id); // error 404
 	}
 	
 }
